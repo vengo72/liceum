37b806bf8cdf93afb8d3b7601f2dac786dba002b
vengo72 <90205884+vengo72@users.noreply.github.com>
2022-10-03 20:38:09 +0500

    Случайная строка из файла — 2

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import sys
 
 from PyQt5.QtWidgets import QApplication, QMainWindow
-from PyQt5 import QtCore, QtGui, QtWidgets
-from calc import Ui_Form
+from test import Ui_Form
+from random import randint
+import os
 
 
 class Example(QMainWindow, Ui_Form):
@@ -10,86 +11,22 @@ class Example(QMainWindow, Ui_Form):
         super().__init__()
         self.setupUi(self)
 
-        self.t = ''
-        self.sp = []
-        self.btn0.clicked.connect(self.butt)
-        self.btn1.clicked.connect(self.butt)
-        self.btn2.clicked.connect(self.butt)
-        self.btn3.clicked.connect(self.butt)
-        self.btn4.clicked.connect(self.butt)
-        self.btn5.clicked.connect(self.butt)
-        self.btn6.clicked.connect(self.butt)
-        self.btn7.clicked.connect(self.butt)
-        self.btn8.clicked.connect(self.butt)
-        self.btn9.clicked.connect(self.butt)
-        self.btn_eq.clicked.connect(self.butt)
-        self.btn_plus.clicked.connect(self.butt)
-        self.btn_div.clicked.connect(self.butt)
-        self.btn_dot.clicked.connect(self.butt)
-        self.btn_minus.clicked.connect(self.butt)
-        self.btn_mult.clicked.connect(self.butt)
-        self.btn_pow.clicked.connect(self.butt)
-        self.btn_sqrt.clicked.connect(self.butt)
-        self.btn_fact.clicked.connect(self.butt)
-        self.btn_clear.clicked.connect(self.butt)
-
-    def butt(self):
-        if str(self.sender().text()) == 'С':
-            self.t = ''
-            self.sp = []
-            self.table.display('0')
-        elif str(self.sender().text()) == '√':
-            self.sqrt()
-        elif str(self.sender().text()) == '!':
-            self.sp.append(self.t[:-1])
-            self.sp.append(self.t[-1])
-            self.fact()
-        else:
-            if str(self.sender().text()) not in '=':
-                if self.t == '0':
-                    self.t = ''
-                self.t += str(self.sender().text())
-                self.table.display(self.t)
-                if self.t[-1] not in '1234567890.':
-                    self.sp.append(self.t[:-1])
-                    self.sp.append(self.t[-1])
-                    self.t = ''
-                    self.table.display('0')
-            elif str(self.sender().text()) == '=':
-                self.run()
+        self.pushButton.clicked.connect(self.run)
 
     def run(self):
-        d = ''
-        for t in self.sp:
-            if t == '^':
-                t = '**'
-            d += t
-        d = d + self.t
-        if '/0' in d:
-            self.table.display('error')
-        else:
-            self.table.display(eval(d))
-
-    def sqrt(self):
-        d = ''
-        for t in self.t:
-            d += t
-        f = int(d)**0.5
-        self.table.display(f)
-
-    def fact(self):
-        d = ''
-        for t in self.sp:
-            if t == '^':
-                t = '**'
-            d += t
-        g = eval(d)
-        r = 1
-        for i in range(1, int(g) + 1):
-            r *= i
-
-        self.table.display(r)
-
+        with open('lines.txt', 'r') as f:
+            lines = f.readlines()
+            a = len(lines)
+            if os.stat("lines.txt").st_size == 0:
+                pass
+            else:
+                b = randint(0, a - 1)
+                f.seek(0)
+                for number, line in enumerate(f):
+                    if b == number:
+                        self.lineEdit.setText(line)
+                        break
+        f.close()
 
 
 if __name__ == '__main__':
@@ -97,3 +34,7 @@ if __name__ == '__main__':
     ex = Example()
     ex.show()
     sys.exit(app.exec())
+
+
+
+
